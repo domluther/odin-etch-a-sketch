89a5cc739f77823baf472c28eb421492eb9fb8a1
dom <35171942+domluther@users.noreply.github.com>
2023-04-13 16:00:30 +0100

    add event handling and buttons

--- README.md
+++ README.md
@@ -8,3 +8,15 @@ https://www.theodinproject.com/lessons/foundations-etch-a-sketch
 - Use a for loop to add the correct number of pixels to that container div
 - Use CSS to wrap the pixels and make them a % of the container size
 - Use a random RGB function to colour each pixel differently to prove that it works.
+
+# Part 2 - Generation
+
+- Add interactive feature - dragging over pixels
+- Pen colours - black/rainbow/white
+- Random generation - like a mosaic
+- Reset image to clear it again
+- Size restarting
+
+# TODO
+
+- Currently changes the pixel each time the mouse moves - how can I set it so it just does one per pixel? challenge - only one event listener not one per pixel
--- script.js
+++ script.js
@@ -1,7 +1,23 @@
 const mainContainerEle = document.querySelector(".mainContainer");
+const settingsContainerEle = document.querySelector(".settings");
+const sizeSetterEle = document.querySelector("#size");
+
+// Track app state / settings
+const state = {
+  mouseDown: false,
+  mode: "black",
+  gridSize: 5,
+};
 
 const randomBetween = (min, max) =>
   min + Math.floor(Math.random() * (max - min + 1));
+
+// Create the appropriate pixel depending on the mode
+const generateRGB = function () {
+  if (state.mode === "rainbow") return generateRandomRGB();
+  if (state.mode === "black") return (rgb = `rgb(0,0,0)`);
+  if (state.mode === "eraser") return (rgb = `rgb(255,255,255)`);
+};
 
 // Used for random pixel colouring mode
 const generateRandomRGB = function () {
@@ -12,26 +28,82 @@ const generateRandomRGB = function () {
 };
 
 // Draw the original layout grid
-const drawPixels = function (pixelsPerSide) {
+const drawPixelsHTML = function () {
+  const pixelsPerSide = state.gridSize;
+  mainContainerEle.innerHTML = ""; // Could use a while loop but faster as no event listeners
+  const pixelHTML = `<div class="pixel" style="width: ${
+    100 / pixelsPerSide
+  }%; background-color: white;"></div>`;
+  // Alternate - create an array of strings and forEach through it
   for (let i = 0; i < pixelsPerSide * pixelsPerSide; i++) {
-    // Inside the loop so it creates a new RGB value each time. Otherwise could be before loop for template
-    const pixelHTML = `<div class="pixel" style="width: ${
-      100 / pixelsPerSide
-    }%; background-color: ${generateRandomRGB()};"></div>`;
+    // Mosaic mode - generates random pixels
+    // const pixelHTML = `<div class="pixel" style="width: ${
+    //   100 / pixelsPerSide
+    // }%; background-color: ${generateRandomRGB(pixelsPerSide)};"></div>`;
+
     mainContainerEle.insertAdjacentHTML("beforeend", pixelHTML);
   }
 };
 
-mainContainerEle.addEventListener("click", function (e) {
-  console.log(`Clicked on ${e.target}`);
+const drawPixelsHTMLRandom = function () {
+  const pixelsPerSide = state.gridSize;
+  console.log("random mode");
+  mainContainerEle.innerHTML = ""; // Could use a while loop but faster as no event listeners
+  for (let i = 0; i < pixelsPerSide * pixelsPerSide; i++) {
+    // Mosaic mode - generates random pixels
+    const pixelHTML = `<div class="pixel" style="width: ${
+      100 / pixelsPerSide
+    }%; background-color: ${generateRandomRGB(pixelsPerSide)};"></div>`;
+    mainContainerEle.insertAdjacentHTML("beforeend", pixelHTML);
+  }
+};
+
+// Only want mouse move to trigger if the mouse is down - ie drag mouse
+mainContainerEle.addEventListener("mousedown", function (e) {
+  state.mouseDown = true;
 });
 
-drawPixels(50);
+mainContainerEle.addEventListener("mouseup", function (e) {
+  state.mouseDown = false;
+});
 
-const generatePixelElement = function () {
-  const newPixel = document.createElement("div");
-  newPixel.classList.add("pixel");
-  newPixel.style.width = `${100 / pixelsPerSide}%`;
-  newPixel.style.backgroundColor = generateRandomRGB();
-  return newPixel;
-};
+mainContainerEle.addEventListener("mousemove", function (e) {
+  const pixel = e.target.closest(".pixel");
+  // Could combine into a NAND but this is clearer guard clause?
+  if (!pixel) return;
+  if (!state.mouseDown) return;
+
+  // Only change the colour if you've clicked on a pixel AND the mouse is down
+  pixel.style.backgroundColor = generateRGB();
+});
+
+settingsContainerEle.addEventListener("click", function (e) {
+  const btn = e.target.closest(".button");
+  if (!btn) return;
+
+  if (e.target.dataset.mode === "magic") {
+    drawPixelsHTMLRandom();
+    return;
+  }
+
+  if (e.target.dataset.mode === "reset") {
+    drawPixelsHTML();
+    return;
+  }
+  state.mode = e.target.dataset.mode;
+});
+
+sizeSetterEle.addEventListener("change", function (e) {
+  console.log(e);
+  e.preventDefault();
+  state.gridSize = e.target.value <= 100 ? e.target.value : 100;
+  sizeSetterEle.value = state.gridSize;
+  drawPixelsHTML();
+});
+
+sizeSetterEle.addEventListener("submit", function (e) {
+  console.log(e);
+  e.preventDefault();
+  drawPixelsHTML(e.target.value);
+});
+drawPixelsHTML();
